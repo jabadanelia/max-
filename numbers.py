c5cc36583ee66d8d0af4193eddef713c4871dd8e
jaba danelia <jabadanelia86@gmail.com>
2023-06-15 21:34:50 +0400

    Numbers sorter

---
+++ numbers.py
@@ -0,0 +1,35 @@
+ 
+n = int(input("How much numbers want enter? "))
+    
+
+num = []
+
+for i in range(n):
+    
+    f = int(input("Please enter some whole numbers: "))
+    num.append(f)
+
+
+new_num = num.copy()
+
+mksort = []
+
+j = 0
+
+while j <= len(num):
+    
+    if len(new_num) > 1:
+        minimum = min(new_num)
+        mksort.append(minimum)
+        new_num.remove(minimum)
+        
+
+    elif len(new_num) == 1:
+        mksort.append(new_num[0])
+        new_num.remove(new_num[0])
+
+   
+
+    j += 1
+
+print(mksort)
